81108569e537291f03512fb408a23f0bd1860320
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-14 22:05:03 +0530

    JDBC Utils

---
+++ src/com/lti/utils/JDBCUtils.java
@@ -0,0 +1,50 @@
+package com.lti.utils;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class JDBCUtils {
+	private static String jdbcURL = "jdbc:oracle:thin:@localhost:1522:OSE";
+    private static String jdbcUsername = "system";
+    private static String jdbcPassword = "99system";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    public static Date getSQLDate(LocalDate date) {
+        return java.sql.Date.valueOf(date);
+    }
+
+    public static LocalDate getUtilDate(Date sqlDate) {
+        return sqlDate.toLocalDate();
+    }
+}

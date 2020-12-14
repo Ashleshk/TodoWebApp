34a1d5241303f375b3fa06bd6461553995eb9f08
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-14 23:25:53 +0530

    JDBC Utils

--- src/com/lti/utils/JDBCUtils.java
+++ src/com/lti/utils/JDBCUtils.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class JDBCUtils {
-	private static String jdbcURL = "jdbc:oracle:thin:@localhost:1522:OSE";
-    private static String jdbcUsername = "system";
-    private static String jdbcPassword = "99system";
+	private static String jdbcURL = "jdbc:mysql://localhost:3306/todoApp";
+    private static String jdbcUsername = "root";
+    private static String jdbcPassword = "root";
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
         } catch (SQLException e) {
             e.printStackTrace();
